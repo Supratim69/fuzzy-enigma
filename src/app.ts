21e2f079dd5330284d3c9fa46eb3e482bd539c7d
Supratim Ghose <supratim7ghose@gmail.com>
2025-10-15 02:41:48 +0530

    feat: build error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import helmet from "helmet";
+import { default as helmet } from "helmet";
 import compression from "compression";
 import morgan from "morgan";
 import dotenv from "dotenv";
